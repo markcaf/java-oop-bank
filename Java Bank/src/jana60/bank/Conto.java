955e2b37b6aea7effe364077ebd0889926d3f5b8
Marco Cafasso <marcocafassog@gmail.com>
2022-11-18 10:41:08 +0100

    feat: add getters and setters

--- Java Bank/src/jana60/bank/Conto.java
+++ Java Bank/src/jana60/bank/Conto.java
@@ -11,5 +11,33 @@ public class Conto {
 		this.ownerName = ownerName;
 		this.bankBalance = 0;
 	}
+
+	/**
+	 * @return the ownerName
+	 */
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+	/**
+	 * @param ownerName the ownerName to set
+	 */
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+
+	/**
+	 * @return the bankAccount
+	 */
+	public int getBankAccount() {
+		return bankAccount;
+	}
+
+	/**
+	 * @return the bankBalance
+	 */
+	public int getBankBalance() {
+		return bankBalance;
+	}
 	
 }
